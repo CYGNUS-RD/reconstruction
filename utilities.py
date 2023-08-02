ccda8b8b9dad5aa6eeb04b83c085a78fc028af65
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2023-08-02 23:54:26 +0200

    go back to the setPedestal original method, which I fixed counting from the back in the manual parsing. I don't know if the auto CSV is cleaned for this kind of possible problem

--- utilities.py
+++ utilities.py
@@ -295,7 +295,7 @@ class utils:
                         pedestal_flag = int(row[-12]) # count from the end, because the field [1] is a txt run description that sometimes has ","...
                     else:
                         pedestal_flag = (":PED:" in runtype)
-                    nevents = int(row[-2]) if str(row[-2]).strip()!="NULL" else 0
+                    nevents = int(row[-2]) if (str(row[-2]).strip()not in ["NULL",'']) else 0
                     if int(runkey)<=int(options.run) and pedestal_flag and nevents>=100:
                         options.pedrun = int(runkey)
                         print("Will use pedestal run %05d which has comment: '%s' and n of events: '%d'" % (int(runkey),comment,int(nevents)))
